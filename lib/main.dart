eeb0c12301bf6b42033820abb091e9362e186b7b
pepix <pepix2112@gmail.com>
2018-03-11 18:32:51 +0900

    step3

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,16 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
     @override
-    Widget build(BuildContext context){
-        final wordPair = new WordPair.random();
-        return new MaterialApp(
+    Widget build(BuildContext context) {
+        return new MaterialApp (
             title: 'Welcome to Flutter App',
-            home: new Scaffold(
-                appBar: new AppBar(
+            home: new Scaffold (
+                appBar: new AppBar (
                     title: new Text('Welcome to Flutter Application')
                 ),
-                body: new Center(
+                body: new Center (
                     /*
                     child: new Text(
                         'Hello World',
@@ -23,9 +22,23 @@ class MyApp extends StatelessWidget{
                         )
                     )
                     */
-                    child: new Text(wordPair.asPascalCase)
+                    // child: new Text(wordPair.asPascalCase)
+                    child: new RandomWords(),
                 ),
             ),
         );
+    }
+}
+
+class RandomWords extends StatefulWidget {
+    @override
+    createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+    @override
+    Widget build(BuildContext context) {
+        final wordPair = new WordPair.random();
+        return new Text(wordPair.asPascalCase);
     }
 }
